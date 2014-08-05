3c98356ec3fff68e24dfa0896277b5f41b1a9746
John <johnmarksjr@gmail.com>
2014-08-05 12:11:24 -0600

    Cleanup
    Added $scope.init() methods in each controller. Helped w/ DRY
    Removed old commented out code in template.

--- waitstaff.js
+++ waitstaff.js
@@ -3,9 +3,7 @@ angular.module('ngWaitstaffApp', [])
 
 .controller('waitstaffCtrl', function($scope) {
     $scope.$on('updateChargesAndEarnings', function(event,data) {
-        console.log("In updateChargesAndEarnings: data = " + data);
-        $scope.$broadcast('updateCharges', data);
-        $scope.$broadcast('updateEarnings', data);
+        $scope.$broadcast('updateCtrl', data);
     });
     $scope.resetCalc = function() {
         $scope.$broadcast('resetCtrl', true);
@@ -13,14 +11,18 @@ angular.module('ngWaitstaffApp', [])
 })
 
 .controller('mealFormCtrl', function($scope, DEFAULT_TAX_RATE) {
-    $scope.data = {bmp: null, taxRate:DEFAULT_TAX_RATE, tipPcnt:null};
-    $scope.submitted = false;
-    $scope.error = 'none';
+    $scope.data = {};
+    $scope.init = function() {
+        $scope.data.bmp = $scope.data.tipPcnt = null;
+        $scope.data.taxRate = DEFAULT_TAX_RATE;
+        $scope.submitted = false;
+        $scope.error = 'none';
+        $('#bmp').focus();
+    }
     $scope.submit = function() {
         if($scope.mealForm.$valid) {
             $scope.$emit('updateChargesAndEarnings', $scope.data);
             $scope.resetForm();
-            $scope.submitted = false;
         } else {
             if ($scope.mealForm.$error.required) {
                 $scope.error = 'required';
@@ -34,38 +36,42 @@ angular.module('ngWaitstaffApp', [])
         }
     }
     $scope.resetForm = function() {
-        $scope.data.bmp = $scope.data.tipPcnt = null;
+        $scope.init();
         $scope.mealForm.$setPristine();
-        $scope.error = 'none';
-        $('#bmp').focus();
     }
     $scope.$on('resetCtrl', function(event, data) {
         $scope.resetForm();
     });
+    $scope.init();
 })
 
 .controller('chargesCtrl', function($scope) {
-    $scope.subtotal = $scope.tip = $scope.total = null;
-    $scope.$on('updateCharges', function(event, data) {
+    $scope.init = function() {
+        $scope.subtotal = $scope.tip = $scope.total = null;
+    }
+    $scope.$on('updateCtrl', function(event, data) {
         $scope.subtotal = data['bmp'] + (data['bmp'] * data['taxRate']/100);
         $scope.tip = data['bmp'] * data['tipPcnt']/100;
         $scope.total = $scope.subtotal + $scope.tip;
     });
     $scope.$on('resetCtrl', function(event, data) {
-        $scope.subtotal = $scope.tip = $scope.total = null;
+        $scope.init()
     });
+    $scope.init();
 })
 
 .controller('earningsCtrl', function($scope, $rootScope) {
-    $scope.tipTotal = $scope.avgTPM = null;
-    $scope.mealCount = 0;
-    $scope.$on('updateEarnings', function(event, data) {
+    $scope.init = function() {
+        $scope.tipTotal = $scope.avgTPM = null;
+        $scope.mealCount = 0;
+    }
+    $scope.$on('updateCtrl', function(event, data) {
         $scope.tipTotal += data['bmp'] * data['tipPcnt']/100;
         $scope.mealCount += 1;
         $scope.avgTPM = $scope.tipTotal / $scope.mealCount;
     });
     $scope.$on('resetCtrl', function(event, data) {
-        $scope.tipTotal = $scope.avgTPM = null;
-        $scope.mealCount = 0;
+        $scope.init();
     });
+    $scope.init();
 })
